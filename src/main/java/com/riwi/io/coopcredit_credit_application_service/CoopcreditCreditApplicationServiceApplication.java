e4a1cf1d8510fe0083c0a378d2d0168bff8f28b4
Julian <julianmoralescolorado49@gmail.com>
2025-12-09 07:59:53 -0500

    Initial commit

---
+++ src/main/java/com/riwi/io/coopcredit_credit_application_service/CoopcreditCreditApplicationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.riwi.io.coopcredit_credit_application_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoopcreditCreditApplicationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoopcreditCreditApplicationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/riwi/io/coopcredit_credit_application_service/CoopcreditCreditApplicationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riwi.io.coopcredit_credit_application_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoopcreditCreditApplicationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
